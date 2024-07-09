2404ca1d8c3016396b83f5153206020a5d63d5f2
Parham Oveissi <73338335+Parham-Oveissi@users.noreply.github.com>
2024-07-09 15:01:45 -0400

    Add files via upload

---
+++ Lorenz_NN_EKF_dlnet.m
@@ -0,0 +1,338 @@
+clc; clear; 
+close all
+
+% Loading the training and the test data sets
+addpath DataSets\Lorenz\
+addpath NN_Functions\
+load('lorenz_train_data_dt_0_01_nst_100_T_10.mat')
+load('lorenz_test_data_dt_0_01_T_10.mat')
+
+input_size  = size(x_data,2); 
+output_size = size(y_data,2); 
+hidden_size = 10;
+
+% Define network architecture
+layers = [
+    featureInputLayer(input_size, 'Normalization', 'none', 'Name', 'input')
+    fullyConnectedLayer(hidden_size, 'Name', 'fc1')
+    reluLayer('Name', 'relu1')
+    fullyConnectedLayer(hidden_size, 'Name', 'fc2')
+    reluLayer('Name', 'relu2')
+    fullyConnectedLayer(hidden_size, 'Name', 'fc3')
+    reluLayer('Name', 'relu3')
+    fullyConnectedLayer(output_size, 'Name', 'fc4')
+];
+
+lgraph = layerGraph(layers);
+dlnet = dlnetwork(lgraph);
+
+% Train, Validation and Test Data
+train_idx = floor(size(x_data,1) * 0.8);
+
+train_inputs  = x_data(1:train_idx, :);
+train_targets = y_data(1:train_idx, :);
+
+val_inputs  = x_data(train_idx+1:end, :);
+val_targets = y_data(train_idx+1:end, :);
+
+test_inputs  = x_test;
+test_targets = y_test;
+
+% Training options
+epochs = 100;
+batch_size = 32;
+learning_rate_init = 0.001;
+validation_freq = 30;
+shuffle = true;
+
+% Training the Model
+% [dlnet,num_iters,trainLoss,valLoss] = train_net_adam(dlnet, train_inputs, train_targets, val_inputs, val_targets, ...
+%     epochs, batch_size, learning_rate_init, validation_freq, shuffle);
+% 
+% save('Lorenz_trained_network_dlnet.mat', 'dlnet')
+% save('Lorenz_train_val_loss_dlnet.mat', 'trainLoss', 'valLoss', 'num_iters')
+
+% Using a trained model
+load("Lorenz_trained_network_dlnet.mat")
+load("Lorenz_train_val_loss_dlnet.mat")
+
+%% Model Prediction Test
+%%% testing the model on the test data. Every sample is picked up from the
+%%% test data set. 
+% Convert test inputs to dlarray
+dlTestX = dlarray(test_inputs', 'CB');
+
+% Predict using the trained network
+dlPredictedOutputs = predict(dlnet, dlTestX);
+predicted_outputs = gather(extractdata(dlPredictedOutputs))';
+
+% Plot the results
+% figure
+% set(gcf, 'Position', [200, 100, 1000, 700])
+% 
+% subplot(3, 1, 1)
+% set(gca,'fontsize',16);
+% plot(test_targets(:, 1), 'g', 'LineWidth', 2)
+% hold on
+% plot(predicted_outputs(:, 1), 'b--', 'LineWidth', 2)
+% ylabel('$x_1$', Interpreter='latex', fontsize=22)
+% grid on
+% legend('$x_1$', '$\hat x_1$', Location='northwest', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+% title('Predictions')
+% 
+% subplot(3, 1, 2)
+% set(gca,'fontsize',16);
+% plot(test_targets(:, 2), 'g', 'LineWidth', 2)
+% hold on
+% plot(predicted_outputs(:, 2), 'b--', 'LineWidth', 2)
+% ylabel('$x_2$', Interpreter='latex', fontsize=22)
+% grid on
+% legend('$x_2$', '$\hat x_2$', Location='northwest', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+% 
+% subplot(3, 1, 3)
+% set(gca,'fontsize',16);
+% plot(test_targets(:, 3), 'g', 'LineWidth', 2)
+% hold on
+% plot(predicted_outputs(:, 3), 'b--', 'LineWidth', 2)
+% ylabel('$x_3$', Interpreter='latex', fontsize=22)
+% grid on
+% legend('$x_3$', '$\hat x_3$', Location='northwest', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+%% EKF Augmentation
+
+% Initial states
+x_dim = input_size;
+num_samples = size(test_inputs, 1);
+x_est = test_inputs(1, :)';
+P = 1e-4 * eye(x_dim);
+
+% Process and measurement noise
+process_var = 0.001;
+Q =  process_var*eye(x_dim);   
+measurement_var = 0.01;
+R = measurement_var;
+
+% Initilization
+x_est_store = zeros(x_dim, num_samples);
+C = [1 0 0];
+y_tot = zeros(size(C,1), num_samples);
+estimation_error = zeros(x_dim, num_samples);
+estimation_error_norm_squared = zeros(1, num_samples);
+J_cost = zeros(1, num_samples);
+eigs = zeros(x_dim, num_samples);
+
+
+% Estimation using EKF
+count = 0;
+for k = 1:num_samples
+    inputData = dlarray(x_est, 'CB');
+    [output, jacobian] = dlfeval(@computeJacobian, dlnet, inputData);
+    A = jacobian;
+
+    eigs(:,k) = eig(A); 
+    if norm(eigs(1,k))<1 && norm(eigs(2,k))<1 && norm(eigs(3,k))<1
+        count = count + 1;
+    end
+
+    dlNextState = predict(dlnet, inputData);    % Prior estimate
+    x_est = gather(extractdata(dlNextState));
+
+    [K, P, J_cost(k)] = Compute_KPJ_EKF(A, C, Q, R, P);      % EKF: Prior & Posterior P and K caculation
+
+    y = C*test_targets(k,:)' + sqrt(measurement_var)*randn*1;    % Measurement
+    y_tot(k) = y;
+
+    % x_est = x_est + K*(y - C*x_est);               % Posterior estimate
+
+    estimation_error(:, k) = abs(x_est - test_targets(k,:)');   % Estimation Error
+    estimation_error_norm_squared(:, k) = norm(x_est - test_targets(k,:)')^2; % Estimation Error Norm Squared
+
+    x_est_store(:, k) = x_est;
+end
+
+%% Plots
+
+figure
+set(gcf,'position',[200,100,800,700])
+subplot(4,1,1)
+plot(test_targets(:,1),'k', LineWidth=2)
+hold on
+plot(x_est_store(1,:),'b--', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_1$', Interpreter='latex', fontsize=22)
+grid on 
+% ylim([-25 20])
+axis tight
+legend('$x_{1,k}$', '$\hat x_{1,k|k}$', Location='best', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+subplot(4,1,2)
+plot(test_targets(:,2),'k', LineWidth=2)
+hold on
+plot(x_est_store(2,:),'b--', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_2$', Interpreter='latex', fontsize=22)
+grid on
+% ylim([-35 20])
+axis tight
+legend('$x_{2,k}$', '$\hat x_{2,k|k}$', Location='best', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+subplot(4,1,3)
+plot(test_targets(:,3),'k', LineWidth=2)
+hold on
+plot(x_est_store(3,:),'b--', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_3$', Interpreter='latex', fontsize=22)
+grid on
+% ylim([0 60])
+axis tight
+legend('$x_{3,k}$', '$\hat x_{3,k|k}$', Location='best', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+subplot(4,1,4)
+semilogy(estimation_error_norm_squared,'b', LineWidth=2)
+set(gca,'fontsize',16);
+xlabel('$k$', Interpreter='latex', fontsize=22)
+grid on
+set(gca, 'YTick', 10.^(-11:2:10))
+ylim([1e-5 1e7])
+hold on
+semilogy(J_cost,'r', LineWidth=2)
+legend('${||e_{k|k}||}^{2}$', '${\rm{tr}} (P_{k|k})$', Location='best', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_NN_EKF')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_NN_EKF')
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_NN')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_NN')
+
+% training and validation losses
+windowSize = 500;
+
+smoothTrainLoss = movmean(trainLoss, windowSize);
+smoothValLoss = movmean(valLoss, windowSize);
+
+figure;
+semilogy((1:1:numel(smoothTrainLoss))*validation_freq, smoothTrainLoss, 'r-', 'DisplayName', 'Training Loss', 'LineWidth',2);
+hold on;
+semilogy((1:1:numel(smoothValLoss))*validation_freq, smoothValLoss, 'b-', 'DisplayName', 'Validation Loss', 'LineWidth',2);
+xlabel('Iteration');
+ylabel('Loss');
+axis tight
+legend;
+grid on;
+
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_training_process')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_training_process')
+
+
+%% Plots for proposal
+
+figure
+plot3(test_inputs(1, 1), test_inputs(1, 2), test_inputs(1, 3), 'ro', 'LineWidth',2)
+hold on
+plot3(test_targets(:,1), test_targets(:,2), test_targets(:,3),'k')
+plot3(x_est_store(1,:), x_est_store(2,:), x_est_store(3,:),'b--')
+grid on
+view(15,20)
+% x_est_store_w_EKF = x_est_store;
+% x_est_store_wo_EKF = x_est_store; 
+% save('x_est_store_w_EKF_file.mat', 'x_est_store_w_EKF')
+% save('x_est_store_wo_EKF_file.mat', 'x_est_store_wo_EKF')
+
+
+figure
+plot3(test_targets(1, 1), test_targets(1, 2), test_targets(1, 3), 'ro', 'LineWidth',2)
+hold on
+plot3(test_targets(:,1), test_targets(:,2), test_targets(:,3),'k')
+grid on
+view(15,20)
+xlim([-20 20])
+ylim([-30 30])
+zlim([0 60])
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_dt_0_01_T_30_traj')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_dt_0_01_T_30_traj')
+
+
+figure
+plot3(x_est_store(1, 1), x_est_store(2,1), x_est_store(3,1), 'ro', 'LineWidth',2)
+hold on
+plot3(x_est_store(1,:), x_est_store(2,:), x_est_store(3,:),'b')
+grid on
+view(15,20)
+xlim([-20 20])
+ylim([-30 30])
+zlim([0 60])
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_NN_EKF_dt_0_01_T_30_traj')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_NN_EKF_dt_0_01_T_30_traj')
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_NN_dt_0_01_T_30_traj')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_NN_dt_0_01_T_30_traj')
+
+%%% If you remove the close all you can plot the true, NN and NN_EKF
+%%% trajectories on the same pot (you also need to give the same numebr to the figure functions, i.e. figure(5))
+
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_NN_NNEKF_dt_0_01_T_30_traj')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_NN_NNEKF_dt_0_01_T_30_traj')
+
+
+figure(10)
+set(gcf,'position',[200,100,800,900])
+subplot(5,1,1)
+plot(test_targets(:,1),'g', LineWidth=2)
+hold on
+plot(x_est_store(1,:),'r', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_1$', Interpreter='latex', fontsize=22)
+grid on 
+% ylim([-25 20])
+axis tight
+
+subplot(5,1,2)
+plot(test_targets(:,2),'g', LineWidth=2)
+hold on
+plot(x_est_store(2,:),'r', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_2$', Interpreter='latex', fontsize=22)
+grid on
+% ylim([-35 20])
+axis tight
+
+subplot(5,1,3)
+plot(test_targets(:,3),'g', LineWidth=2)
+hold on
+plot(x_est_store(3,:),'r', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_3$', Interpreter='latex', fontsize=22)
+grid on
+% ylim([0 60])
+axis tight
+
+subplot(5,1,4)
+semilogy(estimation_error_norm_squared,'b', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+% xlabel('$k$', Interpreter='latex', fontsize=22)
+grid on
+set(gca, 'YTick', 10.^(-11:2:10))
+ylim([1e-5 1e7])
+hold on
+% semilogy(J_cost,'r', LineWidth=2)
+% legend('${||e_{k|k}||}^{2}$', '${\rm{tr}} (P_{k|k})$', Location='best', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+
+subplot(5,1,5)
+% semilogy(estimation_error_norm_squared,'b', LineWidth=2)
+semilogy(J_cost,'r', LineWidth=2)
+set(gca,'fontsize',16);
+xlabel('$k$', Interpreter='latex', fontsize=22)
+grid on
+set(gca, 'YTick', 10.^(-11:2:10))
+ylim([1e-5 1e7])
+hold on
+% legend('${||e_{k|k}||}^{2}$', '${\rm{tr}} (P_{k|k})$', Location='best', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+% print(gcf,'-dpng','Figures/Lorenz/png/Lorenz_NN_NNEKF')
+% print(gcf,'-depsc','Figures/Lorenz/eps/Lorenz_NN_NNEKF')
---
+++ VDP_NN_EKF_dlnet.m
@@ -0,0 +1,197 @@
+clc; clear; close all
+
+% Loading the training and the test data sets
+addpath DataSets\VDP\
+addpath NN_Functions\
+load('VDP_train_data_tf_0_1_ns_30000.mat')
+load('VDP_test_data_tf_0_1_nt_500.mat')
+
+input_size  = size(x_data,2); 
+output_size = size(y_data,2); 
+hidden_size = 10;
+
+% Define network architecture
+layers = [
+    featureInputLayer(input_size, 'Normalization', 'none', 'Name', 'input')
+    fullyConnectedLayer(hidden_size, 'Name', 'fc1')
+    reluLayer('Name', 'relu1')
+    fullyConnectedLayer(hidden_size, 'Name', 'fc2')
+    reluLayer('Name', 'relu2')
+    fullyConnectedLayer(output_size, 'Name', 'fc3')
+];
+
+lgraph = layerGraph(layers);
+dlnet = dlnetwork(lgraph);
+
+% Train, Validation and Test Data
+train_idx = floor(size(x_data,1) * 0.8);
+
+train_inputs  = x_data(1:train_idx, :);
+train_targets = y_data(1:train_idx, :);
+
+val_inputs  = x_data(train_idx+1:end, :);
+val_targets = y_data(train_idx+1:end, :);
+
+test_inputs  = x_test;
+test_targets = y_test;
+
+% Training options
+epochs = 100;
+batch_size = 32;
+learning_rate_init = 0.001;
+validation_freq = 30;
+
+% Training the Model
+% [dlnet,num_iters,trainLoss,valLoss] = train_net_adam(dlnet, train_inputs, train_targets, val_inputs, val_targets, ...
+%     epochs, batch_size, learning_rate_init, validation_freq);
+% 
+% save('VDP_trained_network_dlnet.mat', 'dlnet')
+% save('VDP_train_val_loss_dlnet.mat', 'trainLoss', 'valLoss', 'num_iters')
+
+% Using a trained model
+load("VDP_trained_network_dlnet.mat")
+load("VDP_train_val_loss_dlnet.mat")
+
+%% Model Prediction Test
+% Convert test inputs to dlarray
+dlTestX = dlarray(test_inputs', 'CB');
+
+% Predict using the trained network
+dlPredictedOutputs = predict(dlnet, dlTestX);
+predicted_outputs = gather(extractdata(dlPredictedOutputs))';
+
+% Plot the results
+figure
+set(gcf, 'Position', [200, 100, 1000, 700])
+
+subplot(2, 1, 1)
+set(gca,'fontsize',16);
+plot(test_targets(:, 1), 'g', 'LineWidth', 2)
+hold on
+plot(predicted_outputs(:, 1), 'b--', 'LineWidth', 2)
+ylabel('$x_1$', Interpreter='latex', fontsize=22)
+grid on
+legend('$x_1$', '$\hat x_1$', Location='northwest', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+title('Predictions')
+
+subplot(2, 1, 2)
+set(gca,'fontsize',16);
+plot(test_targets(:, 2), 'g', 'LineWidth', 2)
+hold on
+plot(predicted_outputs(:, 2), 'b--', 'LineWidth', 2)
+ylabel('$x_2$', Interpreter='latex', fontsize=22)
+grid on
+legend('$x_2$', '$\hat x_2$', Location='northwest', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+%% EKF Augmentation
+
+% Initial states
+x_dim = input_size;
+num_samples = size(test_inputs, 1);
+x_est = test_inputs(1, :)';
+P = 1e-4 * eye(x_dim);
+
+% Process and measurement noise
+process_var = 0.0001;
+Q =  process_var*eye(x_dim);   
+measurement_var = 0.01;
+R = measurement_var;
+
+% Initilization
+x_est_store = zeros(x_dim, num_samples);
+C = [1 0];
+y_tot = zeros(size(C,1), num_samples);
+estimation_error = zeros(x_dim, num_samples);
+estimation_error_norm_squared = zeros(1, num_samples);
+J_cost = zeros(1, num_samples);
+eigs = zeros(x_dim, num_samples);
+
+
+% Estimation using EKF
+count = 0;
+for k = 1:num_samples
+    inputData = dlarray(x_est, 'CB');
+    [output, jacobian] = dlfeval(@computeJacobian, dlnet, inputData);
+    A = jacobian;
+
+    eigs(:,k) = eig(A); 
+    if norm(eigs(1,k))<1 && norm(eigs(2,k))<1
+        count = count + 1;
+    end
+
+    dlNextState = predict(dlnet, inputData);    % Prior estimate
+    x_est = gather(extractdata(dlNextState));
+
+    [K, P, J_cost(k)] = Compute_KPJ_EKF(A, C, Q, R, P);      % EKF: Prior & Posterior P and K caculation
+
+    y = C*test_targets(k,:)' + sqrt(measurement_var)*randn;    % Measurement
+    y_tot(k) = y;
+
+    x_est = x_est + K*(y - C*x_est);               % Posterior estimate
+
+    estimation_error(:, k) = abs(x_est - test_targets(k,:)');   % Estimation Error
+    estimation_error_norm_squared(:, k) = norm(x_est - test_targets(k,:)')^2; % Estimation Error Norm Squared
+
+    x_est_store(:, k) = x_est;
+end
+
+%% Plots
+
+figure
+set(gcf,'position',[200,100,800,700])
+subplot(3,1,1)
+plot(test_targets(:,1),'k', LineWidth=2)
+hold on
+plot(x_est_store(1,:),'b--', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_1$', Interpreter='latex', fontsize=22)
+grid on 
+ylim([-2.5 4])
+legend('$x_{1,k}$', '$\hat x_{1,k|k}$', Location='northwest', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+subplot(3,1,2)
+plot(test_targets(:,2),'k', LineWidth=2)
+hold on
+plot(x_est_store(2,:),'b--', LineWidth=2)
+set(gca,'fontsize',16);
+set(gca,'xticklabel',{[]})
+ylabel('$x_2$', Interpreter='latex', fontsize=22)
+grid on
+ylim([-3.5 5])
+legend('$x_{2,k}$', '$\hat x_{2,k|k}$', Location='northwest', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+subplot(3,1,3)
+semilogy(estimation_error_norm_squared,'b', LineWidth=2)
+set(gca,'fontsize',16);
+xlabel('$k$', Interpreter='latex', fontsize=22)
+grid on
+set(gca, 'YTick', 10.^(-11:2:10))
+ylim([1e-5 1e5])
+hold on
+semilogy(J_cost,'r', LineWidth=2)
+legend('${||e_{k|k}||}^{2}$', '${\rm{tr}} (P_{k|k})$', Location='best', box = 'off', NumColumns=2, Interpreter='latex', fontsize=18)
+
+% print(gcf,'-dpng','Figures/VDP/png/VDP_NN_EKF')
+% print(gcf,'-depsc','Figures/VDP/eps/VDP_NN_EKF')
+% print(gcf,'-dpng','Figures/VDP/png/VDP_NN')
+% print(gcf,'-depsc','Figures/VDP/eps/VDP_NN')
+
+% training and validation losses
+windowSize = 500;
+
+smoothTrainLoss = movmean(trainLoss, windowSize);
+smoothValLoss = movmean(valLoss, windowSize);
+
+figure;
+semilogy((1:1:numel(smoothTrainLoss))*validation_freq, smoothTrainLoss, 'r-', 'DisplayName', 'Training Loss', 'LineWidth',2);
+hold on;
+semilogy((1:1:numel(smoothValLoss))*validation_freq, smoothValLoss, 'b-', 'DisplayName', 'Validation Loss', 'LineWidth',2);
+xlabel('Iteration');
+ylabel('Loss');
+axis tight
+legend;
+grid on;
+
+% print(gcf,'-dpng','Figures/VDP/png/VDP_training_process')
+% print(gcf,'-depsc','Figures/VDP/eps/VDP_training_process')
